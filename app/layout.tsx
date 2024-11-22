455857a8eed3d6c03c5f91b650b2d9327864dd3b
Elijah Sippel <37519236+ecuber@users.noreply.github.com>
2024-11-22 15:32:14 -0500

    whitespace begone

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,33 @@
 import type { Metadata } from "next";
-import { Anuphan } from 'next/font/google'
+import { Viga, Mako, Kanit } from "next/font/google";
 import "./globals.css";
 import Navigation from "@/components/nav";
 
-const anuphan = Anuphan({
+const viga = Viga({
   subsets: ["latin"],
   fallback: ["sans-serif"],
-  variable: "--font-anuphan"
-})
+  weight: "400",
+  variable: "--font-viga",
+});
+
+const mako = Mako({
+  subsets: ["latin"],
+  fallback: ["sans-serif"],
+  weight: "400",
+  variable: "--font-mako",
+});
+
+const kanit = Kanit({
+  subsets: ["latin"],
+  fallback: ["sans-serif"],
+  weight: ["400", "700"],
+  variable: "--font-kanit",
+});
 
 export const metadata: Metadata = {
   title: "Flagway League",
-  description: "Flagway exists to create environments where students can practice and celebrate learning math.",
+  description:
+    "Flagway exists to create environments where students can practice and celebrate learning math.",
 };
 
 export default function RootLayout({
@@ -22,7 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${anuphan.variable} antialiased`}
+        className={`${viga.variable} ${mako.variable} ${kanit.variable} antialiased`}
       >
         <Navigation />
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -12,16 +12,25 @@ export default function Home() {
         <Hero />
         <Container>
           <div className="float-right md:w-[45rem] mb-10">
-            <h1>Some landing page content will go here.</h1>
-            <p className="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p className="text-xl">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2>Some landing page content will go here.</h2>
+            <p className="text-xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p className="text-xl">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+            </p>
           </div>
           {/* <div className="relative w-full">
             <SideImage src={Breaker} width={380} alt="student running victoriously" position="left" />
           </div> */}
         </Container>
-      </main >
-
-    </div >
-  )
+      </main>
+    </div>
+  );
 }
